167e187edb40640aea53d218c8a35c8e8360516d
Sas4aZ <paudelsashwat16@gmail.com>
2024-03-21 21:15:26 +0545

    Success alert different added

--- BMI_calculator/js/BMI_calculator.js
+++ BMI_calculator/js/BMI_calculator.js
@@ -30,41 +30,40 @@ function calculateBMI() {
 
         if (bmi < 18.5) {
             result += "You are underweight.";
-
-            displayHealthyWeight(height, weight,gender);
+            displayHealthyWeight(height, weight,gender, 'primary');
         } else if (bmi >= 18.5 && bmi < 25) {
             result += "You have a normal weight.";
             createAlert('add', 'success' , 'Hooray! You are in perfect shape.');
         } else if (bmi >= 25 && bmi < 30) {
             result += "You are overweight.";
-            displayHealthyWeight(height, weight,gender);
+            displayHealthyWeight(height, weight,gender, 'warning');
         } else {
             result += "You are obese.";
-            displayHealthyWeight(height, weight, gender);
+            displayHealthyWeight(height, weight, gender, 'danger');
         }
 
     } else if (gender === "female") {
         if (bmi < 18.5) {
 
             result += "You are underweight.";
-            displayHealthyWeight(height, weight,gender);
+            displayHealthyWeight(height, weight,gender,'primary' );
         } else if (bmi >= 18.5 && bmi < 24) {
             result += "You have a normal weight.";
             createAlert('add', 'success' , 'Hooray! You are in perfect shape.');
         } else if (bmi >= 25 && bmi < 30) {
 
             result += "You are overweight.";
-            displayHealthyWeight(height,weight,gender);
+            displayHealthyWeight(height,weight,gender, 'warning');
         } else {
             result += "You are obese.";
-            displayHealthyWeight(height, weight, gender);
+            displayHealthyWeight(height, weight, gender, 'danger');
 
         }
     }
 
     document.getElementById("result").innerText = result;
 }
-function displayHealthyWeight(height, weight, gender) {
+function displayHealthyWeight(height, weight, gender, alert_type) {
     var healthyBMI;
 
     if (gender === "male") {
@@ -83,7 +82,7 @@ function displayHealthyWeight(height, weight, gender) {
 
     // Display new result
 
-    createAlert('add', 'primary', result);
+    createAlert('add', alert_type, result);
 
     // document.getElementById("healthyWeight").innerText = result;
 }
